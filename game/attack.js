5cb8afefa8e7383a7e25594671706402b358742d
StylishPhoenix <an007567@gmail.com>
2023-03-17 14:59:23 -0500

    Create attack.js
    Moving attack.js to a function instead of a slash command.  I need to add enemy health to this function as well so that a player can continue fighting the same enemy.

---
+++ game/attack.js
@@ -0,0 +1,55 @@
+// commands/attack.js
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { getRandomEnemy } = require('../game/enemies');
+const { getPlayerByUserId, updatePlayerHealth } = require('../playerData');
+
+function calculateDamage(attacker, defender) {
+  const damage = Math.max(attacker.attack - defender.defense, 1);
+  return damage;
+}
+
+async function attack(userId, interaction) {
+    getPlayerByUserId(userId, async (error, player) => {
+      if (error || !player) {
+        console.error(error);
+        return interaction.reply({ content: 'There was an error while retrieving your character!', ephemeral: true });
+      }
+
+      const enemy = getRandomEnemy();
+
+      // Player attacks enemy
+      const playerDamage = calculateDamage(player, enemy);
+      enemy.health -= playerDamage;
+
+      let message = `You dealt ${playerDamage} damage to the enemy ${enemy.name}.`;
+
+      if (enemy.health <= 0) {
+        message += `\nYou defeated the enemy ${enemy.name}!`;
+      } else {
+        // Enemy attacks player
+        const enemyDamage = calculateDamage(enemy, player);
+        player.health -= enemyDamage;
+
+        message += `\nThe enemy ${enemy.name} dealt ${enemyDamage} damage to you.`;
+
+        if (player.health <= 0) {
+          message += `\nYou have been defeated by the enemy ${enemy.name}.`;
+        } else {
+          message += `\nYour health is now ${player.health}.`;
+
+       // Save player's updated health to the database
+       updatePlayerHealth(userId, player.health, (updateError) => {
+            if (updateError) {
+              console.error(updateError);
+            }
+          });
+        }
+      }
+
+      await interaction.reply(message);
+    });
+  }
+
+module.exports = {
+    attack,
+};

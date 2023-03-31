03d0a8062469214db04025bcd63d7f27fe582019
StylishPhoenix <an007567@gmail.com>
2023-03-31 11:47:17 -0500

    Create revive.js

---
+++ game/revive.js
@@ -0,0 +1,21 @@
+const { getPlayerByUserId, updatePlayerHealth } = require('../playerData');
+
+async function revive(interaction, userId, player) {
+  // Define the amount of health to restore when reviving
+  const reviveHealth = Math.floor(player.maxHealth * 0.5);
+
+  // Update player's health in the database
+  updatePlayerHealth(userId, reviveHealth, (error) => {
+    if (error) {
+      console.error(error);
+      return interaction.editReply({ content: 'There was an error while reviving your character!', ephemeral: true });
+    }
+  });
+
+  // Notify the player that they have been revived
+  await interaction.editReply(`You have been revived! Your health is now ${reviveHealth}.`);
+}
+
+module.exports = {
+  revive,
+};
